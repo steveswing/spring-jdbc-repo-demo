bb24cb3787cbf5045d0266e1bdfb7645b244f99b
Steve Swing <steve.swing@gmail.com>
2019-12-23 16:48:42 -0500

    Converging.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.demo.config.H2TestProfileJPAConfig;
+import com.example.demo.config.H2TestProfileJdbcConfig;
 import com.example.demo.config.TestEmailConfig;
 
-@SpringBootTest(classes = {TestEmailConfig.class, H2TestProfileJPAConfig.class, DemoApplication.class})
+@SpringBootTest(classes = {TestEmailConfig.class, H2TestProfileJdbcConfig.class, DemoApplication.class})
 public class DemoApplicationTests {
 
     @Test
